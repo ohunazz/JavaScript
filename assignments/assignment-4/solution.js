aaf65aa319291d4fd4ff58f034be45d06b1007cd
ohunazz <ohunazz@gmail.com>
2023-06-03 18:39:51 -0400

    Finish Concat Method

--- assignments/assignment-4/solution.js
+++ assignments/assignment-4/solution.js
@@ -10,3 +10,26 @@ var author = " - Nelson Mandela";
 var result = `${part1} ${part2} ${part3} ${part4} ${part5} ${author}`;
 
 console.log(result);
+
+// The Concat() Method //
+
+var part1 = "The greatest glory in";
+var part2 = "living lies not in";
+var part3 = "never falling, but in";
+var part4 = "rising every time";
+var part5 = "we fall.";
+var author = " - Nelson Mandela";
+
+var result = part1.concat(
+    " ",
+    part2,
+    " ",
+    part3,
+    " ",
+    part4,
+    " ",
+    part5,
+    " ",
+    author
+);
+console.log(result);
