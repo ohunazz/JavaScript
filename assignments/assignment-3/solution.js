2da792756b69252570bd2d3ac8d7a8ae22647a71
ohunazz <ohunazz@gmail.com>
2023-06-03 17:20:55 -0400

    Finish Part 2

---
+++ assignments/assignment-3/solution.js
@@ -0,0 +1,25 @@
+// Basic Arithmetic Operations //
+
+var num1 = 15;
+var num2 = 5;
+
+var add = num1 + num2;
+console.log(add);
+var sub = num1 - num2;
+console.log(sub);
+var multip = num1 * num2;
+console.log(multip);
+var divide = num1 / num2;
+console.log(divide);
+
+// Temperature Converter //
+
+// var fahrenheit = 110;
+// var celsius = (fahrenheit - 32) * (5 / 9);
+// console.log(celsius);
+
+var celsius = 60;
+var fahrenheit = celsius * (9 / 5) + 32;
+console.log(fahrenheit); // 160
+
+//  Interest Calculator //
