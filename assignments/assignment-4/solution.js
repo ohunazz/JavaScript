6b25ffc3e526eb2fa5f45b39d030e356de0c7ab2
ohunazz <ohunazz@gmail.com>
2023-06-03 18:45:33 -0400

    Finish Operator Method

--- assignments/assignment-4/solution.js
+++ assignments/assignment-4/solution.js
@@ -33,3 +33,27 @@ var result = part1.concat(
     author
 );
 console.log(result);
+
+// + Operator //
+
+var part1 = "The greatest glory in";
+var part2 = "living lies not in";
+var part3 = "never falling, but in";
+var part4 = "rising every time";
+var part5 = "we fall.";
+var author = " - Nelson Mandela";
+
+var result =
+    part1 +
+    " " +
+    part2 +
+    " " +
+    part3 +
+    " " +
+    part4 +
+    " " +
+    part5 +
+    " " +
+    author;
+
+console.log(result);
