e66868c9c97f5da0b526d85d967cda0d0cb0b1e2
ohunazz <ohunazz@gmail.com>
2023-06-05 11:39:30 -0400

    Finish Assignment-3

--- assignments/assignment-3/solution.js
+++ assignments/assignment-3/solution.js
@@ -23,3 +23,32 @@ var fahrenheit = celsius * (9 / 5) + 32;
 console.log(fahrenheit); // 160
 
 //  Interest Calculator //
+
+var interest = (principal * rate * time) / 100;
+var principal = 20000;
+var rate = 3.9;
+var time = 5;
+
+console.log(interest);
+
+// Swapping Values //
+
+var a = 9;
+var b = 4;
+console.log("Before swapping: a =", a, ", b =", b);
+
+a = a + b;
+b = a - b;
+a = a - b;
+console.log("After swapping: a =", a, ", b =", b);
+
+// Implementing Mathematical Formulas //
+
+var radius = 5;
+var area = 3.14 * radius * radius;
+console.log("Area of circle:", area);
+
+var length = 10;
+var width = 5;
+var perimeter = 2 * (length + width);
+console.log("Perimeter of rectangle:", perimeter);
