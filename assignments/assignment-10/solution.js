5eb83092dfaf8e00f7655d5c0229a11a794bb5c8
Okhunjon Azzamkulov <129907532+ohunazz@users.noreply.github.com>
2023-06-08 22:17:04 -0400

    Delete assignments/assignment-10 directory

--- assignments/assignment-10/solution.js
+++
@@ -1,9 +0,0 @@
-// Part-1 //
-
-var randomSeconds = Math.floor(Math.random() * 86400);
-console.log(randomSeconds);
-
-var hours = Math.floor(randomSeconds / 3600);
-var leftSeconds = randomSeconds % 3600;
-var minutes = leftSeconds / 60;
-var seconds = leftSeconds % 60;
