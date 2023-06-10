0cf1256cefbff24010955031a918ddfa977027bc
ohunazz <ohunazz@gmail.com>
2023-06-10 13:00:34 -0400

    Finish Assignment-11

---
+++ assignments/assignment-11/solution.js
@@ -0,0 +1,125 @@
+// part-1 //
+
+function addTwo(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+
+console.log("4 + 9=", addTwo(4, 9));
+
+// pat-2 //
+
+function multiplyTwo(num1, num2) {
+    var product = num1 * num2;
+    return product;
+}
+
+console.log("4 * 15=", multiplyTwo(4, 15));
+
+// par-3 //
+
+function isOdd(num) {
+    var sum = num % 2 !== 0;
+    return sum;
+}
+
+console.log(7 % 2 !== 0);
+
+// part-4 //
+
+function subtractTwo(num1, num2) {
+    var difference = num1 - num2;
+    return difference;
+}
+
+console.log("15 - 7", subtractTwo(15, 7));
+
+// part-5 //
+
+function divideTwo(num1, num2) {
+    var quotent = num1 / num2;
+    return quotent;
+}
+
+console.log("90 / 3", divideTwo(90, 3));
+
+//part-6 //
+
+function isEven(num) {
+    var sum = num % 2 === 0;
+    return sum;
+}
+
+console.log(isEven(12));
+
+// part-7 //
+
+function maxValue(num1, num2) {
+    return Math.max(num1, num2);
+}
+
+console.log(Math.max(14, 27));
+
+// part-8 //
+
+function minValue(num1, num2) {
+    return Math.min(num1, num2);
+}
+
+console.log(Math.min(8, 23));
+
+// part-9 //
+
+function absoluteValue(num) {
+    return Math.abs(number);
+}
+
+console.log(Math.abs(-7));
+
+// part-10 //
+
+function roundNumber(num) {
+    return Math.round(num);
+}
+
+console.log(Math.round(7.5));
+
+// part-11 //
+
+function ceilNumber(num) {
+    return Math.ceil(num);
+}
+
+console.log(Math.ceil(5.2));
+
+// part-12  //
+
+function floorNumber(num) {
+    return Math.floor(num);
+}
+
+console.log(Math.floor(7.8));
+
+//  part-13 //
+
+function modulus(num1, num2) {
+    return num1 % num2;
+}
+
+console.log(modulus(8, 2));
+
+// part-14 //
+
+function power(num, exponent) {
+    return Math.pow(num, exponent);
+}
+
+console.log(Math.pow(7, 5));
+
+// part-15 //
+
+function squareRoot(num) {
+    return Math.sqrt(num);
+}
+
+console.log(Math.sqrt(7));
