f78be71a2f98930e156897f1c0b0919677e28216
ohunazz <ohunazz@gmail.com>
2023-07-12 16:56:36 -0400

    Finish Assignment

--- assignments/assignment-24/solution.js
+++ assignments/assignment-24/solution.js
@@ -1,23 +1,11 @@
-// 1 //
-
-//  Creating User Profiles:
-
-const userProfile = {
-    name: "Jane Doe",
-    age: 28,
-    email: "jane@example.com",
-    isAdmin: true
-};
+// 1. Creating User Profiles
 function printUserProfile(userProfile) {
     for (let key in userProfile) {
         console.log(`${key}: ${userProfile[key]}`);
     }
 }
 
-// 2 //
-
-// Updating Inventory
-
+// 2. Updating Inventory
 function addStock(inventory, newInventory) {
     for (let item in newInventory) {
         if (inventory[item] !== undefined) {
@@ -29,89 +17,92 @@ function addStock(inventory, newInventory) {
     return inventory;
 }
 
-// console.log(
-//     addStock(
-//         { apple: 5, banana: 3, orange: 2 },
-//         { banana: 2, orange: 3, peach: 5 }
-//     )
-// );
-// console.log(addStock({ apple: 10, orange: 5 }, { apple: 5, banana: 10 }));
-
-// 3 //
-
-// Filtering Properties
-
+// 3. Filtering Properties
 function filterData(object, key) {
     return { [key]: object[key] };
 }
 
-// console.log(
-//     filterData(
-//         {
-//             city: "New York",
-//             state: "New York",
-//             country: "USA",
-//             pincode: "10001"
-//         },
-//         "state"
-//     )
-// );
-// console.log(
-//     filterData(
-//         {
-//             city: "Los Angeles",
-//             state: "California",
-//             country: "USA",
-//             pincode: "90001"
-//         },
-//         "city"
-//     )
-// );
-
-// 4 //
-
-// Checking the Perfect Score:
-
+// 4. Checking the Perfect Score
 function perfectScore(students) {
     for (let i = 0; i < students.length; i++) {
-        if (students[i].score === 100) {
+        const student = students[i];
+        if (student.score === 100) {
             return true;
         }
     }
     return false;
 }
 
-// console.log(
-//     perfectScore([
-//         { name: "John", score: 98 },
-//         { name: "Jane", score: 100 },
-//         { name: "Bob", score: 95 }
-//     ])
-// );
-// console.log(
-//     perfectScore([
-//         { name: "Adam", score: 95 },
-//         { name: "Eve", score: 99 }
-//     ])
-// );
-
-// 5 //
-
-//Counting Occurrences:
-
+// 5. Counting Occurrences
 function countOccurrence(str) {
-    let countChars = {};
-
+    let count = {};
     for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        if (countChars[char]) {
-            countChars[char]++;
+        const char = str[i];
+        if (count[char]) {
+            count[char]++;
         } else {
-            countChars[char] = 1;
+            count[char] = 1;
         }
     }
-    return countChars;
+    return count;
 }
 
-// console.log(countOccurrence("banana"));
-// console.log(countOccurrence("apple"));
+// 6. Calculating Average Score
+function calculateAverage(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum / numbers.length;
+}
+
+function averageScore(scores) {
+    let averageScores = {};
+    for (let student in scores) {
+        const averageScore = calculateAverage(scores[student]);
+        averageScores[student] = averageScore;
+    }
+    return averageScores;
+}
+
+// 7. Finding Largest Number
+function findLargest(ages) {
+    let oldest = "";
+    let maxAge = 0;
+    for (let person in ages) {
+        if (ages[person] > maxAge) {
+            maxAge = ages[person];
+            oldest = person;
+        }
+    }
+    return oldest;
+}
+
+// 8. Reversing Key-Value Pairs
+function reverseKeyValue(object) {
+    let reversed = {};
+    for (let key in object) {
+        reversed[object[key]] = key;
+    }
+    return reversed;
+}
+
+// 9. Counting Boolean Values
+function countBooleans(array) {
+    let count = { true: 0, false: 0 };
+    for (let i = 0; i < array.length; i++) {
+        const value = array[i];
+        count[value]++;
+    }
+    return count;
+}
+
+// 10. Calculating Total Price
+function calculateTotal(prices, items) {
+    let total = 0;
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
+        total += prices[item];
+    }
+    return total;
+}
